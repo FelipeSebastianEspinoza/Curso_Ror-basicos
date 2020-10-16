5ba2807b53772e0b6877e73cb82dfc7503a77b9f
Usuario <sebastian_felipe1507@hotmail.com>
2020-10-15 21:23:59 -0300

    imagenes y estudio

---
+++ community/app/controllers/images_controller.rb
@@ -0,0 +1,48 @@
+class ImagesController < ApplicationController
+  before_action :set_image, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @image = Image.new
+  end
+
+  def index
+    @images = Image.all
+  end
+
+  def create
+    # render plain: params[:image].inspect
+    @image = Image.new image_params
+    @image.save
+    redirect_to @image
+  end
+
+  def show
+    #  @image = Image.find params[:id]
+  end
+
+  def edit
+    #   @image = Image.find params[:id]
+  end
+
+  def destroy
+    #   @image = Image.find params[:id]
+    @image.destroy
+    redirect_to images_path
+  end
+
+  def update
+    #  @image = Image.find params[:id]
+    @image.update image_params
+    redirect_to @image
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:description)
+  end
+
+  def set_image
+    @image = Image.find params[:id]
+  end
+end
